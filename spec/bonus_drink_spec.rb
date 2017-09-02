3e7f70d7eba250c98e8592089db3548b531014e4
itume <jem90ham@gmail.com>
2017-09-02 20:01:45 +0900

    必要なテストを追加

--- spec/bonus_drink_spec.rb
+++ spec/bonus_drink_spec.rb
@@ -1,5 +1,26 @@
 require File.expand_path(File.dirname(__FILE__) + '/../bonus_drink')
 
 describe BonusDrink do
-  specify { expect(BonusDrink.total_count_for(0)).to eq 0 }
+  describe '#total_count_for' do
+    it '購入した本数が0本なら、0本飲める' do
+      quantity = 0
+      expect(BonusDrink.total_count_for(quantity)).to eq 0
+    end
+    xit '購入した本数が1本なら、1本飲める' do
+      quantity =  1
+      expect(BonusDrink.total_count_for(quantity)).to eq 1
+    end
+    xit '購入した本数が3本なら、4本飲める' do
+      quantity = 3
+      expect(BonusDrink.total_count_for(quantity)).to eq 4
+    end
+    xit '購入した本数が11本なら、16本飲める' do
+      quantity = 11
+      expect(BonusDrink.total_count_for(quantity)).to eq 16
+    end
+    xit '購入した本数が100本なら、飲めるのは0本' do
+      quantity = 100
+      expect(BonusDrink.total_count_for(quantity)).to eq
+    end
+  end
 end
