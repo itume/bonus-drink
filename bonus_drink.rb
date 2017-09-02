d2480a0e7915820a7b5d0146280014d008ba1b71
itume <jem90ham@gmail.com>
2017-09-02 20:45:53 +0900

    テストが通るように実装

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,8 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    return 0 if amount == 0
+    # 最初の一本を除いて2で割ると、もらえる本数がわかる
+    gain = (amount - 1) / 2
+    amount + gain
   end
 end
--- spec/bonus_drink_spec.rb
+++ spec/bonus_drink_spec.rb
@@ -6,15 +6,15 @@ describe BonusDrink do
       quantity = 0
       expect(BonusDrink.total_count_for(quantity)).to eq 0
     end
-    xit '購入した本数が1本なら、1本飲める' do
+    it '購入した本数が1本なら、1本飲める' do
       quantity =  1
       expect(BonusDrink.total_count_for(quantity)).to eq 1
     end
-    xit '購入した本数が3本なら、4本飲める' do
+    it '購入した本数が3本なら、4本飲める' do
       quantity = 3
       expect(BonusDrink.total_count_for(quantity)).to eq 4
     end
-    xit '購入した本数が11本なら、16本飲める' do
+    it '購入した本数が11本なら、16本飲める' do
       quantity = 11
       expect(BonusDrink.total_count_for(quantity)).to eq 16
     end
